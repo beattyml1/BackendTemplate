f74c9204bddb2ce861f40936fc2411197b324afb
Matthew Beatty <beattyml1@gmail.com>
2025-02-09 10:18:41 -0500

    Fail on error

--- scripts/test/test-http.sh
+++ scripts/test/test-http.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #npx httpyac --all --verbose tests/component/api/*.http
 #docker run --rm -i -t -v $PWD:/workdir jetbrains/intellij-http-client tests/component/api/test_main.http
+set -e
 find "$(pwd)/tests/component/api" -exec ./scripts/test/ijhttp/ijhttp {} \;
