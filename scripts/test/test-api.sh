874aa72841c8c2946bede9ea18ba576aa346ae72
Matthew Beatty <beattyml1@gmail.com>
2025-02-12 08:56:52 -0500

    helps if you run the right script

--- scripts/test/test-api.sh
+++ scripts/test/test-api.sh
@@ -3,7 +3,7 @@
 #docker run --rm -i -t -v $PWD:/workdir jetbrains/intellij-http-client tests/component/api/test_main.http
 set -e
 
-scripts/run/api.sh &
+nohup scripts/run/api.sh &
 scripts/test/test-http.sh
 process_id=$(ps aux | grep "uvicorn backend_template.apps.api.start:app" | grep -v grep | awk '{print $2}')
 kill $process_id
